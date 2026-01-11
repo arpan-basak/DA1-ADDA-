dc898264234bfe61248dcd1d334fa2da129013a1
Arpan Basak <arpan22082003@gmail.com>
2026-01-11 23:31:38 +0530

    First Commit- Added JAVA Program

---
+++ StudentInfo.java
@@ -0,0 +1,7 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Student Name: Arpan Basak");
+        System.out.println("Register Number: 23MIC0064");
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+    }
+}
