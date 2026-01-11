bf69b28fe1d562af901d0f63784cd7e24f83a890
Arpan Basak <arpan22082003@gmail.com>
2026-01-11 23:36:32 +0530

    Second Commit- Updated JAVA File

--- StudentInfo.java
+++ StudentInfo.java
@@ -3,5 +3,6 @@ public class StudentInfo {
         System.out.println("Student Name: Arpan Basak");
         System.out.println("Register Number: 23MIC0064");
         System.out.println("Message: Jenkins–GitHub Integration Successful");
+        System.out.println("This is after second commit.");
     }
 }
